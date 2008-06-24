2e87b4941af0ce50cb3516702ee43de4f7fa9d41
agraef <agraef@20125082-fe4b-0410-b030-c4beea4ddc84>
2008-06-24 22:41:06 +0000

    Comment changes.

--- pure/trunk/examples/poor.c
+++ pure/trunk/examples/poor.c
@@ -1,20 +1,23 @@
 
 /* Poor man's Pure interpreter. 2008-06-24 AG */
 
-/* This is an example for calling Pure from C/C++ applications. Using the
-   public runtime API of Pure 0.5 or later, it implements a little command
-   loop which reads lines of Pure code from standard input, evaluates them and
-   prints the results, until it encounters end-of-file.
+/* This is an example for calling Pure from a standalone C/C++ application
+   which is *not* hosted by the command line version of the Pure interpreter,
+   but uses the public runtime API of Pure 0.5 or later to create its own
+   interpreter instance. The program implements a little command loop which
+   reads Pure code from standard input, evaluates it and prints the results.
 
    Compile this with 'gcc -o poor poor.c -lpure', and run the resulting
    executable as './poor [args ...]'. You can use the same command line
    arguments as with the real Pure interpreter, including any Pure scripts to
    be loaded at startup. Input is line-oriented, so you can't continue
    definitions across lines, but in return you don't have to terminate each
-   line with a ';' either, the eval() function already takes care of that.
+   line with a ';' either, the eval() function already takes care of that. To
+   terminate the program just type the end-of-file character at the beginning
+   of a line.
 
-   Please note that the interpreter interface provided by the runtime API is
-   rather minimalistic right now. In particular, the interpreter always runs
+   Please note that the interface to interpreter instances created with the
+   runtime API is rather minimalistic right now. The interpreter always runs
    in non-interactive mode (thus none of the interactive commands will work)
    and eval() only returns the result of the last computed expression (this is
    what gets printed in the read-eval-print loop). */
